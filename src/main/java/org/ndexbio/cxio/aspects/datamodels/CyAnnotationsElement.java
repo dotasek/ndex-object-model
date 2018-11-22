8047d78f3c105da95b19281ebd18064c4a7306f1
Unknown <dotasek.dev@gmail.com>
2018-11-22 16:55:25 -0500

    Annotations commit 2

--- src/main/java/org/ndexbio/cxio/aspects/datamodels/CyAnnotationsElement.java
+++ src/main/java/org/ndexbio/cxio/aspects/datamodels/CyAnnotationsElement.java
@@ -1,8 +1,10 @@
 package org.ndexbio.cxio.aspects.datamodels;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
-import org.ndexbio.cxio.util.CxConstants;
 import org.ndexbio.cxio.util.JsonWriter;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -21,89 +23,126 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public final class CyAnnotationsElement extends AbstractAspectElement {
 
-    /**
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -1305940218684987322L;
-	public final static String UUID              ="uuid";
-    public final static String ANNOTATION_NAME = "n";
-    public final static String ASPECT_NAME     = "cyAnnotations";
-    
-	@JsonProperty(CxConstants.ID)
-    private  String        _uuid;
-
-	@JsonProperty(ANNOTATION_NAME)
-    private String        _annotation_name;
+	public final static String ASPECT_NAME = "cyAnnotations";
 	
-	public CyAnnotationsElement () {
-		_uuid = null;
+	public final static String UUID = "uuid";
+	public final static String VIEW = "view";
+	public final static String PROPERTIES = "properties";
+	
+	@JsonProperty(UUID)
+	private String _uuid;
+	
+	@JsonProperty(VIEW)
+	private Long _view;
+
+	 final public Long getView() {
+	        return _view;
+	    }
+	    
+	    public void setView (Long v) { _view = v;}
+	
+	@JsonProperty(PROPERTIES)
+	// @JsonSerialize(using = AttributeValueSerializer.class)
+	private SortedMap<String, String> _properties;
+
+	public final SortedMap<String, String> getProperties() {
+		return _properties;
 	}
-	
 
-    public CyAnnotationsElement(final String id, final String annotation_name) {
-        _uuid = id;
-        _annotation_name = annotation_name;
-    }
+	public void setProperties(SortedMap<String, String> p) {
+		_properties = p;
+	}
 
+	public final void putProperty(final String name, final String value) {
+		_properties.put(name, value);
+	}
 
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) {
-            return true;
-        }
-        return (o instanceof CyAnnotationsElement) && (_uuid.equals(((CyAnnotationsElement) o).getUUID()));
-    }
+	public CyAnnotationsElement() {
+		_uuid = null;
+		_view = null;
+		_properties = new TreeMap<>();
+	}
 
-    @Override
+	public CyAnnotationsElement(final String uuid, long view) {
+		_uuid = uuid;
+		_view = view;
+		_properties = new TreeMap<>();
+	}
+
+	@Override
+	public boolean equals(final Object o) {
+		if (this == o) {
+			return true;
+		}
+		return (o instanceof CyAnnotationsElement) && (_uuid.equals(((CyAnnotationsElement) o).getUUID()));
+	}
+
+	@Override
 	@JsonIgnore
-    final public String getAspectName() {
-        return CyAnnotationsElement.ASPECT_NAME;
-    }
+	final public String getAspectName() {
+		return CyAnnotationsElement.ASPECT_NAME;
+	}
 
 	@JsonIgnore
-    final public String getUUID() {
-        return _uuid;
-    }
-    
-    public void setUUID(String uuid) { _uuid = uuid; }
+	final public String getUUID() {
+		return _uuid;
+	}
 
-	@JsonIgnore
-    public String getAnnotationName() {
-        return _annotation_name;
-    }
-    
-	
-    
-    final public void setAnnotationName( final String annotation_name ) {
-        _annotation_name = annotation_name;
-    }
+	public void setUUID(String uuid) {
+		_uuid = uuid;
+	}
 
-    @Override
-    public int hashCode() {
-        return String.valueOf(_uuid).hashCode();
-    }
+	@Override
+	public int hashCode() {
+		return String.valueOf(_uuid).hashCode();
+	}
 
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder();
-        if (_annotation_name != null) {
-            sb.append(_annotation_name);
-            sb.append(" ");
-        }
-        sb.append("id: ");
-        sb.append(_uuid);
-        return sb.toString();
-    }
+	@Override
+	public String toString() {
+		final StringBuilder sb = new StringBuilder();
+		
+		sb.append("uuid: ");
+		sb.append(_uuid);
+		sb.append("\n");
+		
+		  sb.append("view: ");
+          sb.append(_view);
+          sb.append("\n");
+		
+		sb.append("properties:");
+		sb.append("\n");
+		for (final Map.Entry<String, String> entry : _properties.entrySet()) {
+			sb.append(entry.getKey());
+			sb.append(": ");
+			sb.append(entry.getValue());
+			sb.append("\n");
+		}
+		return sb.toString();
+	}
 
 	@Override
 	public void write(JsonWriter w) throws IOException {
-	       w.writeStartObject();
-	       w.writeStringField("uuid", _uuid);
-	        if (_annotation_name != null) {
-	            w.writeStringField(CyAnnotationsElement.ANNOTATION_NAME, _annotation_name);
-	        }
-	        w.writeEndObject();	 
-	        w.flush();
-	} 
+		final CyAnnotationsElement c = this;
+		w.writeStartObject();
+		w.writeStringField(CyAnnotationsElement.UUID, _uuid);
+		w.writeNumberField(CyVisualPropertiesElement.VIEW, c.getView());
+		
+		if ((c.getProperties() != null) && !c.getProperties().isEmpty()) {
+			w.writeObjectFieldStart(CyAnnotationsElement.PROPERTIES);
+			for (final Map.Entry<String, String> entry : c.getProperties().entrySet()) {
+				if (entry.getValue() != null) {
+					w.writeStringField(entry.getKey(), entry.getValue());
+				}
+			}
+			w.writeEndObject();
+		}
+
+		w.writeEndObject();
+		w.flush();
+	}
 
 }
--- src/main/java/org/ndexbio/cxio/aspects/writers/CyAnnotationsFragmentWriter.java
+++ src/main/java/org/ndexbio/cxio/aspects/writers/CyAnnotationsFragmentWriter.java
@@ -1,6 +1,7 @@
 package org.ndexbio.cxio.aspects.writers;
 
 import java.io.IOException;
+import java.util.Map;
 
 import org.ndexbio.cxio.aspects.datamodels.CyAnnotationsElement;
 import org.ndexbio.cxio.core.interfaces.AspectElement;
@@ -25,10 +26,16 @@ public class CyAnnotationsFragmentWriter extends AbstractFragmentWriter {
         final CyAnnotationsElement e = (CyAnnotationsElement) element;
         w.writeStartObject();
         w.writeStringField(CyAnnotationsElement.UUID, e.getUUID());
-        if (e.getAnnotationName() != null) {
-            w.writeStringField(CyAnnotationsElement.ANNOTATION_NAME, e.getAnnotationName());
-        }
-       
+        w.writeNumberField(CyAnnotationsElement.VIEW, e.getView());
+        if ((e.getProperties() != null) && !e.getProperties().isEmpty()) {
+			w.writeObjectFieldStart(CyAnnotationsElement.PROPERTIES);
+			for (final Map.Entry<String, String> entry : e.getProperties().entrySet()) {
+				if (entry.getValue() != null) {
+					w.writeStringField(entry.getKey(), entry.getValue());
+				}
+			}
+			w.writeEndObject();
+		}
         w.writeEndObject();
     }
 }
